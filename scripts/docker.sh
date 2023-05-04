0dfa1a3968576f8dd889c966c3123c6b6eed7918
Dinçer GÖKSÜLÜK <dincer.goksuluk@gmail.com>
2023-05-04 11:40:52 +0300

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,25 +1,40 @@
-# Install Docker Engine
+# INSTALL DOCKER ENGINE
+# 1. Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 apt-get update
-apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+apt-get install \
+  ca-certificates \
+  curl \
+  gnupg
 
-mkdir -p /etc/apt/keyrings
-
+# 2. Add Docker’s official GPG key:
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
+# 3. Use the following command to set up the repository:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+# 4. Install Docker Engine
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Install Docker Compose (Standalone)
-curl -SL https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+# POST INSTALLATION STEPS
+# 1. Create the docker group.
+groupadd docker
+
+# 2. Add your user to the docker group.
+usermod -aG docker $USER
+
+
+# INSTALL DOCKER COMPOSE (STANDALONE)
+# 1. To download and install Compose standalone, run:
+curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+
+# 2. Apply executable permissions to the standalone binary in the target path for the installation.
 chmod +x /usr/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
